4dabeb9018e90b1dc4331f4980768d8414066007
Lakens <D.Lakens@tue.nl>
2020-07-13 13:41:20 +0200

    update book

--- footer.R
+++ footer.R
@@ -8,10 +8,10 @@ write_custom_footer <- function() {
   writeLines(
     c("<div class=\"row\" style=\"padding-top: 2em;\">",
       "<p style=\"text-align: center\">",
-      "<img src=\"images/worship.jpg\" style=\"width: 100px; padding: 0; display: inline; vertical-align: top\">",
+      "<img src=\"images/logo.png\" style=\"width: 100px; padding: 0; display: inline; vertical-align: top\">",
       "<span style=\"display: inline-block; margin-left: 2em; margin-top: 16px; font-size: small\">",
-      "<span style=\"font-weight: bold;\">Eric P. Green</span><br/>",
-      "<a href=\"https://themethodsection.com\">themethodsection.com</a><br/>",
+      "<span style=\"font-weight: bold;\">Daniel Lakens</span><br/>",
+      "<a href=\"https://statistical-inferences.com\">statistical-inferences.com</a><br/>",
       paste("page built ", as.character(Sys.Date())),
       "</span>",
       "</p>",
